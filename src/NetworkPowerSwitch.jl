998a6ed62fec61903db57216813de12959d254f5
Lixun <lixun.xia@outlook.com>
2018-11-15 22:22:04 +0800

    more flexibility for better control

--- src/NetworkPowerSwitch.jl
+++ src/NetworkPowerSwitch.jl
@@ -59,74 +59,137 @@ function server()
 end
 
 
+
+
+
 """
-    client(to1, to2)
+    npsopen(ip::IPv4=ip"192.168.1.199", port=12345)
+
+open socket to nps
 
 # Arguments
-    - 'to1': time from turnoff-all to first conduct, for example 5 seconds
-    - 'to2': time from turnoff-all to second conduct, for example 20 seconds
+    - 'ip': IPv4 address of the remote switch
+    - 'port': port number of the remote switch
 """
-function client(to1, to2, ip::IPv4=ip"192.168.1.199", port=12345)
+function npsopen(ip::IPv4=ip"192.168.1.199", port=12345)
     root = joinpath(Libaudio.folder(), Libaudio.logfile())
+    sock = connect(ip, port)
+
+    write(sock, [0x41 0x54 0x0D 0x0A])
+    status = readline(sock)
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsopen: AT $(status)")
+
+    write(sock, [0x41 0x54 0x2B 0x4C 0x49 0x4E 0x4B 0x53 0x54 0x41 0x54 0x3D 0x3F 0x0D 0x0A])
+    status = readline(sock)
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsopen: AT+LINKSTAT $(status)")
+    
+    write(sock, [0x41 0x54 0x2B 0x4D 0x4F 0x44 0x45 0x4C 0x3D 0x3F 0x0D 0x0A])
+    status = readline(sock)
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsopen: AT+MODEL: $(status)")
+
+
+    strtemp = "Init Status: "
+    write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x3F 0x0D 0x0A])
+    for i = 1:4
+        status = readline(sock)
+        strtemp = strtemp * status
+    end
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsopen: $(strtemp)")
+    return sock
+end
+
+
+
+
+"""
+    npsoff(sock)
+
+turn off all switches
+"""
+function npsoff(sock)
+    root = joinpath(Libaudio.folder(), Libaudio.logfile())
+    write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x30 0x0D 0x0A])
+    status = readline(sock)
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsoff: turn off all switches $(status)")
+    return nothing
+end
+
+
+
+
+"""
+    npson(sock, i)
+
+turn on i-th switch 
+"""
+function npson(sock, i=1)
+    root = joinpath(Libaudio.folder(), Libaudio.logfile())
+    pa = [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48]
+    pb = [0x3D 0x31 0x0D 0x0A]
+    write(sock, [pa convert(UInt8,0x30+i) pb])
+    status = readline(sock)       
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npson: turn on switch $(i) $(status)")
+    return nothing
+end
+
+
+
+
+"""
+    npsclose(sock)
+
+get the updated register states, close the socket and return true
+"""
+function npsclose(sock)
+    root = joinpath(Libaudio.folder(), Libaudio.logfile())
+    strtemp = "Updated Status: "
+    write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x3F 0x0D 0x0A])
+    for i = 1:4
+        status = readline(sock)
+        strtemp = strtemp * status
+    end
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsclose: $(strtemp)")
+    close(sock)            
+    Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.npsclose: socket closed")
+    return true
+end
+
+
+
+
+"""
+    npserr()
+
+if something is wrong return false
+"""
+function npserr()
+    root = joinpath(Libaudio.folder(), Libaudio.logfile())
+    Libaudio.printl(root, :light_red, Libaudio.nows() * " | NetworkPowerSwitch.npserr: unknown reason")
+    return false
+end
+
+
+
+
+"""
+    reboot(t, ip, port) where T <: Real
+
+Turn off all switches and turn on each switch with time sequence specified.
+# Arguments
+    - 't': time between off and on 
+           or visually, (all switches off)-->t[1]-->(switch 1 on)-->t[2]-->(switch 2 on)-->t[3]...
+"""
+function reboot(t::Array{T,1}) where T <: Real
     try
-        # op = Array{String,1}()
-        sock = connect(ip, port)
-
-        write(sock, [0x41 0x54 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "AT: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: AT $(status)")
-
-        write(sock, [0x41 0x54 0x2B 0x4C 0x49 0x4E 0x4B 0x53 0x54 0x41 0x54 0x3D 0x3F 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "AT+LINKSTAT: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: AT+LINKSTAT $(status)")
-        
-        write(sock, [0x41 0x54 0x2B 0x4D 0x4F 0x44 0x45 0x4C 0x3D 0x3F 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "AT+MODEL: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: AT+MODEL: $(status)")
-
-        strtemp = "Init Status: "
-        write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x3F 0x0D 0x0A])
-        for i = 1:4
-            status = readline(sock)
-            strtemp = strtemp * status
+        nps = npsopen() 
+        npsoff(nps)
+        for (j,k) in enumerate(t)
+            sleep(k)
+            npson(nps, j)
         end
-        # push!(op, strtemp)
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: $(strtemp)")
-
-        write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x30 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "Turn off all switches: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: turn off all switches $(status) and wait for $(to1) seconds")
-        sleep(to1)
-
-        write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x31 0x3D 0x31 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "Turn on switch 1: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: turn on switch 1 $(status) and wait for $(to2) seconds")
-        sleep(to2)
-
-        write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x32 0x3D 0x31 0x0D 0x0A])
-        status = readline(sock)
-        # push!(op, "Turn on switch 2: $status")
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: turn on switch 2 $(status)")
-                
-        strtemp = "Status: "
-        write(sock, [0x41 0x54 0x2B 0x53 0x54 0x41 0x43 0x48 0x30 0x3D 0x3F 0x0D 0x0A])
-        for i = 1:4
-            status = readline(sock)
-            strtemp = strtemp * status
-        end
-        # push!(op, strtemp)
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: $(strtemp)")
-        close(sock)            
-        Libaudio.printl(root, :green, Libaudio.nows() * " | NetworkPowerSwitch.client: power cycle complete")
-        return true
+        npsclose(nps)
     catch
-        Libaudio.printl(root, :light_red, Libaudio.nows() * " | NetworkPowerSwitch.client: unknown error")
-        return false
+        npserr()
     end
 end
 
